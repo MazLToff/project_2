9d196a956c902159082d74bdf7784abcb0314e46
Vlad <vak.2-8@yandex.ru>
2024-01-21 21:59:16 +0500

    фон

--- Flappybird2.py
+++ Flappybird2.py
@@ -6,8 +6,15 @@ width = 1000
 height = 600
 window = pygame.display.set_mode((width, height))
 
-while True:
+background_image = pygame.image.load('images/background.png')
+background_image = pygame.transform.scale(background_image, (width, height))
+
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+    window.blit(background_image, (0, 0))
+    pygame.display.flip()
+
+pygame.quit()
