6bf3f8e234b3dd371255ff623a7f82ecc0d8107b
Dima <Dashevchenko2006@yandex.ru>
2024-01-21 23:28:19 +0500

    ver.006

--- Flappybird2.py
+++ Flappybird2.py
@@ -12,34 +12,20 @@ background_image = pygame.transform.scale(background_image, (width, height))
 
 hero_image = pygame.image.load('images/hero.png')
 hero_image = pygame.transform.scale(hero_image, (80, 45))
-hero_x, hero_y = abs(width / 12), abs(height / 2) - 20,
+hero_x, hero_y = abs(width / 12), abs(height / 2) - 20
 
 post_image = pygame.image.load('images/post.png')
 all_posts = []
 
+gravity = 0.01
+jump_force = -1
+hero_velocity = 0
+jumping = False
 
 def draw_hero():
     window.blit(hero_image, (hero_x, hero_y))
 
-
-def createPipe():
-    offset = height / 3
-    pipeHeight = game_images['pipeimage'][0].get_height()
-    y2 = offset + \
-         random.randrange(
-             0, int(height - game_images['sea_level'].get_height() - 1.2 * offset))
-    pipeX = width + 10
-    y1 = pipeHeight - y2 + offset
-    pipe = [
-        # upper Pipe
-        {'x': pipeX, 'y': -y1},
-
-        # lower Pipe
-        {'x': pipeX, 'y': y2}
-    ]
-    return pipe
-
-def create_post():
+def create_pipe():
     if len(all_posts) == 0 or width - all_posts[-1][1] > 300:
         gm = 200
         post_height = random.randint(50, height - gm - 50)
@@ -49,32 +35,36 @@ def create_post():
         all_posts.append([post1, width, 0])
         all_posts.append([post2, width, post_height + gm])
 
-
-
 def draw_posts():
     for post in all_posts:
         window.blit(post[0], (post[1], post[2]))
         post[1] -= 0.7
     if post[1] < 600:
-        create_post()
+        create_pipe()
 
-
-create_post()
+create_pipe()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                hero_y -= 50
-            if event.key == pygame.K_w:
-                hero_y -= 50
             if event.key == pygame.K_SPACE:
-                hero_y -= 50
-    hero_y += 0.3
+                jumping = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                jumping = False
+
+    if jumping:
+        hero_velocity = jump_force
+    else:
+        hero_velocity += gravity
+
+    hero_y += hero_velocity
+
     if hero_y > height:
-        running = False
+        hero_y = height
+
     window.blit(background_image, (0, 0))
     draw_hero()
     draw_posts()
